272ce9d3101266ed1703365850769cb4e10a992a
codercooler <codercooler@gmail.com>
2025-07-17 14:38:24 +0800

    init

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+uvicorn translate_service:app --host 0.0.0.0 --port 8889
---
+++ translate_service.py
@@ -0,0 +1,28 @@
+import logging
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from pydantic import BaseModel
+from transformers import MarianMTModel, MarianTokenizer
+
+class TranslateRequest(BaseModel):
+    zh: str
+
+logger = logging.getLogger(__name__)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    model_name = "Helsinki-NLP/opus-mt-zh-en"
+    tokenizer = MarianTokenizer.from_pretrained(model_name)
+    model = MarianMTModel.from_pretrained(model_name)
+    app.state.tokenizer = tokenizer
+    app.state.model = model
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/zhToEn")
+async def zhToEn(request: TranslateRequest):
+    inputs = app.state.tokenizer([request.zh], return_tensors="pt", padding=True)
+    translated = app.state.model.generate(**inputs)
+    translated_text = app.state.tokenizer.batch_decode(translated, skip_special_tokens=True)
+    return translated_text[0]
